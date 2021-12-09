2e65118e7669452ef608d777749aba7527c6fec1
Uchiha34 <90205302+Uchiha34@users.noreply.github.com>
2021-12-09 22:14:06 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+git clone -b $branch https://github.com/Zylern/TGVid-Comp /root/Zylern
+cp ultroid/.env /root/Zylern/.env
+cd /root/Zylern
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
