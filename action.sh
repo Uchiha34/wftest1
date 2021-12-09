af4000239bb4464f0f1f3942160e5a82160036dd
Uchiha34 <90205302+Uchiha34@users.noreply.github.com>
2021-12-09 22:16:01 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,5 +2,5 @@ branch=main
 git clone -b $branch https://github.com/Zylern/TGVid-Comp /root/Zylern
 cp ultroid/.env /root/Zylern/.env
 cd /root/Zylern
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t tgvid-comp
+docker run --privileged --env-file .env --rm -i tgvid-comp
